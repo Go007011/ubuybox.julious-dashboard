f28041b7515a839eb5f402b455d872f57d3385d8
Go007011 <108307920+Go007011@users.noreply.github.com>
2026-03-20 14:41:41 -0400

    Modify Vite server configuration
    Updated server configuration to include allowedHosts.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
+ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
-    host: 'localhost',
-  },
+    allowedHosts: [
+      'capital-stack-ui.cluster-0.preview.emergentcf.cloud'
+    ]
+  }
 })
